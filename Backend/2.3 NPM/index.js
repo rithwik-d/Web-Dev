c91bfbd465cbbe1a7251036b2d8caca7c0c20acd
Rithwik Reddy <rkspvn@gmail.com>
2026-03-04 16:27:52 -0600

    Generating a Random Superhero Name

--- Backend/2.3 NPM/index.js
+++ Backend/2.3 NPM/index.js
@@ -1,3 +1,9 @@
-var generateName = require('sillyname');
-var sillyname = generateName();
-console.log("My name is " + sillyname);
+// var generateName = require('sillyname');
+// var sillyname = generateName();
+// console.log("My name is " + sillyname);
+
+import superheroes from "superheroes";
+
+const mySuperheroName = randomSuperhero();
+
+console.log("I am " + mySuperheroName + "!!");
