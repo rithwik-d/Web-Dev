e4df1ad5ef027138493b9c25a035d4cbaab0267f
Rithwik Reddy <rkspvn@gmail.com>
2026-03-04 18:32:04 -0600

    using Nodemon to autosave and restart the server on changes. Updated package.json to include "type": "module" for ES6 module support. Modified index.js to use ES6 import syntax instead of require.

--- Backend/3.1 Express Server/index.js
+++ Backend/3.1 Express Server/index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send("<h1>Welcome to Express Server.</h1>");
     // console.log(req.rawheaders);
 })
 
