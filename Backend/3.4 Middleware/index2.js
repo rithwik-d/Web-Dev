32b5b1872ff8a2fab7d952630bc7e41aae01d2f9
Rithwik Reddy <rkspvn@gmail.com>
2026-03-05 14:15:36 -0600

    Using Middleware's lo log get and post requests and creating custom middlewares.

--- Backend/3.4 Middleware/index2.js
+++ Backend/3.4 Middleware/index2.js
@@ -1,10 +1,12 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
+app.use(morgan("combined"));
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
--- Backend/3.4 Middleware/index3.js
+++ Backend/3.4 Middleware/index3.js
@@ -1,7 +1,14 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
 const port = 3000;
+
+function logger(req, res, next) {
+  console.log("Request Method:", req.method);
+  console.log("Request URL:", req.url);
+  next();
+}
 
 app.use(logger);
 
