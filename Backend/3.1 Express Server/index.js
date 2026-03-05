65e4143091031cd0af9aef857a425c5b27af65d2
Rithwik Reddy <rkspvn@gmail.com>
2026-03-04 18:36:58 -0600

    Creating different endpoints for the server. This includes a GET endpoint for the home page and a POST endpoint for handling form submissions. The GET endpoint will return a simple message, while the POST endpoint will process the incoming data and return a response based on the submitted information.

--- Backend/3.1 Express Server/index.js
+++ Backend/3.1 Express Server/index.js
@@ -8,6 +8,14 @@ app.get('/', (req, res) => {
     // console.log(req.rawheaders);
 })
 
+app.get('/about', (req, res) => {
+    res.send("<h1>About Page</h1>");
+});
+
+app.get('/contact', (req, res) => {
+    res.send("<h1>Contact Page</h1>");
+});
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
